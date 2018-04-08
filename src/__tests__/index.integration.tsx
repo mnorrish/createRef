464d22d3bb146c34a244ecb8fd7a3f024d9677df
Matthew Norrish <matt.norrish@clavisinsight.com>
2018-04-09 09:46:26 +1200

    add more extensive tests which verify behaviour when used in components

---
+++ src/__tests__/index.integration.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { render, wait } from 'react-testing-library';
+import createRef from '..';
+
+test('correctly assigns ref.current for rendered HTML element', () => {
+  const ref = createRef<HTMLDivElement>();
+
+  class A extends React.Component {
+    myRef = ref;
+    render() {
+      return <div data-testid='x' ref={this.myRef} />;
+    }
+  }
+
+  const { getByTestId } = render(<A />);
+
+  expect(ref.current).toBe(getByTestId('x'));
+});
+
+test('correctly assigns ref.current for rendered component', () => {
+  class A extends React.Component {
+    render() {
+      return <div />;
+    }
+  }
+
+  const ref = createRef<A>();
+
+  class B extends React.Component {
+    myRef = ref;
+    render() { return <A ref={this.myRef} /> }
+  }
+
+  const {container} = render(<B />);
+
+  expect(ref.current).toEqual(expect.any(A));
+});
